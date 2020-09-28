39d20121a91daec2f6ac1f38a46645fce4f9e5b2
victoradt <audetat.v@gmail.com>
2020-09-28 23:15:26 +0200

    formating data

--- src/components/mainText.js
+++ src/components/mainText.js
@@ -4,12 +4,12 @@ import gsap from 'gsap';
 const MainText = (props) => {
   const data = [
     {
-      sentence: "Hi",
-      alternative: "Tchô!"
+      sentence: "how was your day",
+      alternative: "fine thanks lol"
     },
     {
-      sentence: "how was your day",
-      alternative: "fabulous"
+      sentence: "how was your lol",
+      alternative: "fabulous mdr"
     }
   ];
 
@@ -28,7 +28,66 @@ const MainText = (props) => {
     }
   };
 
+  let fDataS = []
+  let fDataA = []
+
+  const formatData = (data, maxChar) => {
+    data.map((e, i) => {
+      let sentence = e.sentence.split(" ")
+      let alternative = e.alternative.split(" ")
+      let formatedDataS = { sentence: '' }
+      let formatedDataA = { alternative: '' }
+      fDataS.push([])
+      fDataA.push([])
+      sentence.map((word, ind) => {
+        if (formatedDataS.sentence.length + word.length + 1 < maxChar) {
+          if (formatedDataS.sentence) {
+            formatedDataS.sentence = formatedDataS.sentence + " " + word
+          } else{
+            formatedDataS.sentence = word
+          }
+          if (ind === sentence.length - 1) {
+            fDataS[i].push(formatedDataS)
+            formatedDataS = { sentence: '' }
+          }
+        } else {
+          fDataS[i].push(formatedDataS)
+          formatedDataS = { sentence: '' }
+          formatedDataS.sentence = word
+          if (ind === sentence.length - 1) {
+            fDataS[i].push(formatedDataS)
+            formatedDataS = { sentence: '' }
+          }
+        }
+      })
+      alternative.map((word, ind) => {
+        if (formatedDataA.alternative.length + word.length + 1 < maxChar) {
+          if (formatedDataA.alternative) {
+            formatedDataA.alternative = formatedDataA.alternative + " " + word
+          } else {
+            formatedDataA.alternative = word
+          }
+          if (ind === alternative.length - 1) {
+            fDataA[i].push(formatedDataA)
+            formatedDataA = { alternative: '' }
+          }
+        } else {
+          fDataA[i].push(formatedDataA)
+          formatedDataA = { alternative: '' }
+          formatedDataA.alternative = word
+          if (ind === alternative.length - 1) {
+            fDataA[i].push(formatedDataA)
+            formatedDataA = { alternative: '' }
+          }
+        }
+      })
+    })
+
+    console.log(fDataS);  
+  }
+
   useEffect(() => {
+    formatData(data, 15)
     refs.map((e, i) => {
       console.log(e);
       const tl = gsap.timeline({ paused: true })
@@ -76,19 +135,19 @@ const MainText = (props) => {
   return (
     <div> {props.data.map((data, index) => {
       return (
-          <svg viewBox="-0 -0 100 35" preserveAspectRatio="xMinYMid meet" ref={addRef}>
-            <defs>
-              <filter id={`goo-${index}`}>
-                <feGaussianBlur className="feGau" in="SourceGraphic" stdDeviation={0} result="blur"></feGaussianBlur>
-                <feColorMatrix className="feCol" in="blur" mode="matrix" values="1 0 0 0 0 0 1 0 0 0 1 0 1 0 0 0 0 0 1 0" result="goo"></feColorMatrix>
-                <feComposite in="SourceGraphic" in2="goo" operator="atop"></feComposite>
-              </filter>
-            </defs>
-            <g onMouseEnter={e => mouseIn(e, index)} onMouseLeave={e => mouseOut(e, index)} style={{ filter: `url(#goo-${index})` }}>
-              <text className="sentence" style={{ opacity: '1' }} x="0" y="15"> <tspan>{data.sentence} </tspan><tspan dy={"1em"}  x={0}>asdasd</tspan></text>
-              <text className="alternative" style={{ opacity: '0' }} x="0" y="15"> <tspan>{data.alternative} </tspan> <tspan dy={"1em"}  x={0}>abcde</tspan></text>
-            </g>
-          </svg>
+        <svg viewBox="-0 -0 100 35" preserveAspectRatio="xMinYMid meet" ref={addRef}>
+          <defs>
+            <filter id={`goo-${index}`}>
+              <feGaussianBlur className="feGau" in="SourceGraphic" stdDeviation={0} result="blur"></feGaussianBlur>
+              <feColorMatrix className="feCol" in="blur" mode="matrix" values="1 0 0 0 0 0 1 0 0 0 1 0 1 0 0 0 0 0 1 0" result="goo"></feColorMatrix>
+              <feComposite in="SourceGraphic" in2="goo" operator="atop"></feComposite>
+            </filter>
+          </defs>
+          <g onMouseEnter={e => mouseIn(e, index)} onMouseLeave={e => mouseOut(e, index)} style={{ filter: `url(#goo-${index})` }}>
+            <text className="sentence" style={{ opacity: '1' }} x="0" y="15"> <tspan>{data.sentence} </tspan><tspan dy={"1em"} x={0}>ba</tspan></text>
+            <text className="alternative" style={{ opacity: '0' }} x="0" y="15"> <tspan>{data.alternative} </tspan> <tspan dy={"1em"} x={0}></tspan></text>
+          </g>
+        </svg>
       )
     })} </div>
   );
